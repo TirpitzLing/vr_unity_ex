4dd388341010264ba2f0f591e3e32cc62fe0045d
TirpitzRyou <tirpitzryou@gmail.com>
2025-11-17 11:46:27 +0200

    still errors

--- Assets/NewBehaviorScript.cs
+++ Assets/NewBehaviorScript.cs
@@ -1,11 +1,28 @@
 using UnityEngine;
+using Photon.Pun;
 
 public class NewBehaviorScript : Interactive
 {
     private Color originalColor;
     private bool isBlue = false;
+    private PhotonView pv;
+
+    void Start()
+    {
+        pv = GetComponent<PhotonView>();
+    }
 
     public new void Interact()
+    {
+        if (pv == null)
+        {
+            return; // Only allow interaction by the owner
+        }
+        pv.RPC("ToggleColor", RpcTarget.All);
+    }
+
+    [PunRPC]
+    public void ToggleColor()
     {
         if (!isBlue)
         {
